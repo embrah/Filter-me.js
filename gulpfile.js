c8f8d05bb78fe21dce568d29b9e0520d78bdc7bc
Emil Mladenov <amladenoff@gmail.com>
2016-05-24 21:36:08 +0100

    Added gulp set up
    Added gulp set up with
    
    sass to css conversion
    live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require ('browser-sync').create();
 
 gulp.task('sass',function(){
 	return gulp.src('src/assets/styles/sass/main.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('src/assets/styles/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
 });
 
+gulp.task('watch',['browser-sync','sass'],function(){
+	gulp.watch('src/assets/styles/sass/**/*.sass',['sass']);
+	gulp.watch('src/*.html',browserSync.reload);
+	gulp.watch('src/assets/scripts/*.js',browserSync.reload);
+});
+
+gulp.task('browser-sync',function(){
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		},
+	});
+});
