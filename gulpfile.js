069b815969e631d8b5aac94825d225ba3212e918
Emil Mladenov <amladenoff@gmail.com>
2016-05-24 21:22:01 +0100

    Added initial folder set-up
    Added initial folder set-up and gulp structure

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass',function(){
+	return gulp.src('src/assets/styles/sass/main.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('src/assets/styles/css'))
+});
+
