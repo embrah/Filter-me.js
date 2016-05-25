5e6b461e8da0f8684f1517c278e96655c0dcb3d3
Emil Mladenov <amladenoff@gmail.com>
2016-05-25 06:06:21 +0100

    Added object set up
    Added object set up and side menu concept

---
+++ src/assets/script/main.js
@@ -0,0 +1,30 @@
+//grayscale   - 0 - 100%
+// sepia - 0 - 100%
+//saturate - 0 - 100%
+//hue-rotate - 0 - 360deg
+//invert - 0 - 100%
+//opacity -  0 - 100%
+//brightness - 0 - 100%
+//contrast - 0 - 100%
+//blur - 0 - 50px
+
+
+var effects;
+
+
+effects = {
+	'grayscale' : 0,
+	'sepia' : 0,
+	'saturate' : 0,
+	'hue-rotate' : 0,
+	'invert' : 0,
+	'opacity' : 0,
+	'brightness' : 0,
+	'contrast' : 0,
+	'blur' : 0
+};
+
+
+
+
+
